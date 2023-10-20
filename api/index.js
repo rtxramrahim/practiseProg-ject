dc7c209a9fd1e271770dbbd749bc88c08d899aa3
shashwat <103182850+rtxramrahim@users.noreply.github.com>
2023-10-20 17:29:50 +0530

    database connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose, { mongo } from 'mongoose'
+dotenv.config()
 const app = express()
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT,()=>{
     console.log(`App is up and running at port ${PORT}`)
+})
+
+
+mongoose.connect(process.env.DATABASE_URL).then(()=>{
+    console.log("database connected")
+}).catch(()=>{
+    console.log("error occured while connecting to database!")
+    console.log(err)
 })
